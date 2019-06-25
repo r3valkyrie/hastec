97604ca3948bf026ba9e0a4703ec62c0d9b23d16
r3valkyrie <valkyrie@r3valkyrie.com>
2019-06-25 08:45:07 -0400

    makes code a bit prettier

--- haste.c
+++ haste.c
@@ -14,9 +14,9 @@
  * `cat filename.txt | haste`
  */
 
-#define HASTEBIN_URL "https://haste.r3valkyrie.com" 				/* Hastebin URL */
+#define HASTEBIN_URL "https://haste.r3valkyrie.com"
 
-uint8_t * dyn_mem_allocate(FILE * fp, size_t size)					/* Dynamically allocate enough memory for the user input */
+uint8_t * dyn_mem_allocate(FILE * fp, size_t size)
 {
     uint8_t * str;
     int64_t ch;
@@ -28,8 +28,10 @@ uint8_t * dyn_mem_allocate(FILE * fp, size_t size)					/* Dynamically allocate e
 
     while ((ch = fgetc(fp)) != EOF) {
         str[len++] = ch;
+
         if (len == size) {
             str = realloc(str, sizeof(uint8_t) * (size += 1024));
+
             if (!str) return str;
         }
     }
@@ -39,7 +41,7 @@ uint8_t * dyn_mem_allocate(FILE * fp, size_t size)					/* Dynamically allocate e
     return realloc(str, sizeof(uint8_t) * len);
 }
 
-size_t print_url(void * buffer, size_t size, size_t nmemb, void * userp) 	/* libcurl callback function, prints hastebin URL */
+size_t print_url(void * buffer, size_t size, size_t nmemb, void * userp)
 {
     struct json_object * parsed_json;
     struct json_object * key;
@@ -47,14 +49,12 @@ size_t print_url(void * buffer, size_t size, size_t nmemb, void * userp) 	/* lib
     parsed_json = json_tokener_parse(buffer);
 
     json_object_object_get_ex(parsed_json, "key", & key);
-    printf("\n"
-        HASTEBIN_URL "/%s\n", json_object_get_string(key));
+    printf("\n" HASTEBIN_URL "/%s\n", json_object_get_string(key));
 }
 
 int64_t post_input(uint8_t * dat)
 {
-    uint8_t * url = HASTEBIN_URL "/documents"; 					/* The URL we are sending the POST request to */
-
+    uint8_t * url = HASTEBIN_URL "/documents";
     CURL * curl;
     CURLcode res;
 
@@ -63,26 +63,28 @@ int64_t post_input(uint8_t * dat)
     if (curl) {
         struct curl_slist * headers = NULL;
 
-        curl_easy_setopt(curl, CURLOPT_URL, url); 				/* The URL */
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, dat); 			/* The data being pasted */
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, print_url);		/* Write to callback function */
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, dat);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, print_url);
 
-        res = curl_easy_perform(curl); 						/* Perform the request */
+        res = curl_easy_perform(curl);
 
-        curl_easy_cleanup(curl); 						/* Cleanup */
+        curl_easy_cleanup(curl);
     }
+
     curl_global_cleanup();
+
     return 0;
 }
 
 int64_t main(void)
 {
     uint8_t * dat;
+
     dat = dyn_mem_allocate(stdin, 1024);
 
     post_input(dat);
-
     free(dat);
-    return 0;
 
+    return 0;
 }
