a4113361e0b03f132f387e1f50d252cf75ebd273
r3valkyrie <valkyrie@r3valkyrie.com>
2019-06-25 08:53:39 -0400

    change indentation to 8 spaces

--- haste.c
+++ haste.c
@@ -4,6 +4,8 @@
 #include <json-c/json_object.h>
 #include <curl/curl.h>
 
+#define HASTEBIN_URL "https://haste.r3valkyrie.com"
+
 /*
  * Takes input from stdin and posts it to a specified hastebin.
  *
@@ -14,77 +16,79 @@
  * `cat filename.txt | haste`
  */
 
-#define HASTEBIN_URL "https://haste.r3valkyrie.com"
 
 uint8_t * dyn_mem_allocate(FILE * fp, size_t size)
 {
-    uint8_t * str;
-    int64_t ch;
-    size_t len = 0;
+        uint8_t * str;
+        int64_t ch;
+        size_t len = 0;
 
-    str = realloc(NULL, sizeof(uint8_t) * size);
+        str = realloc(NULL, sizeof(uint8_t) * size);
 
-    if (!str) return str;
+        if (!str) return str;
 
-    while ((ch = fgetc(fp)) != EOF) {
-        str[len++] = ch;
+        while ((ch = fgetc(fp)) != EOF) {
+                str[len++] = ch;
 
-        if (len == size) {
-            str = realloc(str, sizeof(uint8_t) * (size += 1024));
+                if (len == size) {
+                        str = realloc(str, sizeof(uint8_t) * (size += 1024));
 
-            if (!str) return str;
+                        if (!str) return str;
+                }
         }
-    }
 
-    str[len++] = '\0';
+        str[len++] = '\0';
 
-    return realloc(str, sizeof(uint8_t) * len);
+        return realloc(str, sizeof(uint8_t) * len);
 }
+
 
 size_t print_url(void * buffer, size_t size, size_t nmemb, void * userp)
 {
-    struct json_object * parsed_json;
-    struct json_object * key;
+        struct json_object * parsed_json;
+        struct json_object * key;
 
-    parsed_json = json_tokener_parse(buffer);
+        parsed_json = json_tokener_parse(buffer);
 
-    json_object_object_get_ex(parsed_json, "key", & key);
-    printf("\n" HASTEBIN_URL "/%s\n", json_object_get_string(key));
+        json_object_object_get_ex(parsed_json, "key", & key);
+        printf("\n" HASTEBIN_URL "/%s\n", json_object_get_string(key));
 }
+
 
 int64_t post_input(uint8_t * dat)
 {
-    uint8_t * url = HASTEBIN_URL "/documents";
-    CURL * curl;
-    CURLcode res;
+        uint8_t * url = HASTEBIN_URL "/documents";
+        CURL * curl;
+        CURLcode res;
 
-    curl = curl_easy_init();
+        curl = curl_easy_init();
 
-    if (curl) {
-        struct curl_slist * headers = NULL;
+        if (curl) {
+                struct curl_slist * headers = NULL;
 
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, dat);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, print_url);
+                curl_easy_setopt(curl, CURLOPT_URL, url);
+                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, dat);
+                curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, print_url);
 
-        res = curl_easy_perform(curl);
+                res = curl_easy_perform(curl);
 
-        curl_easy_cleanup(curl);
-    }
+                curl_easy_cleanup(curl);
+        }
 
-    curl_global_cleanup();
+        curl_global_cleanup();
 
-    return 0;
+        return 0;
 }
+
 
 int64_t main(void)
 {
-    uint8_t * dat;
+        uint8_t * dat;
 
-    dat = dyn_mem_allocate(stdin, 1024);
+        dat = dyn_mem_allocate(stdin, 1024);
 
-    post_input(dat);
-    free(dat);
+        post_input(dat);
+        free(dat);
 
-    return 0;
+        return 0;
 }
